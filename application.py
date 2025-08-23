54d9ca20494136162f25b40f0613526ab71b6608
root <root@Mohit.localdomain>
2025-08-23 06:08:56 +0000

    Initial commit - Flask MVP Task Manager

---
+++ application.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:StrongPassword123@taskdb.cpu2ei8ogsps.us-east-1.rds.amazonaws.com:3306/taskdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.String(20), default='Pending')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    tasks = Task.query.order_by(Task.created_at.desc()).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    if title:
+        new_task = Task(title=title)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>')
+def update(id):
+    task = Task.query.get_or_404(id)
+    task.status = 'Done' if task.status == 'Pending' else 'Pending'
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# for Elastic Beanstalk
+application = app
